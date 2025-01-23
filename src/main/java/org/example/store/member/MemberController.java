81a8953699c9714f4f353d9c890e08fb434a3669
leehansae1 <crof672@naver.com>
2025-01-23 17:54:24 +0900

    Delete src/main/java/org/example/store/member directory

--- src/main/java/org/example/store/member/MemberController.java
+++
@@ -1,8 +0,0 @@
-package org.example.store.member;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MemberController {
-    
-}
--- src/main/java/org/example/store/member/MemberService.java
+++
@@ -1,4 +0,0 @@
-package org.example.store.member;
-
-public class MemberService {
-}
