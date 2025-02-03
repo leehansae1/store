cc697b2baf33be2253f23b3375b996432e9b907a
leehansae1 <crof672@naver.com>
2025-02-03 16:07:11 +0900

    product package delete

--- src/main/java/org/example/store/product/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.store.product.repository;
-
-import org.example.store.product.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<Image, Integer> {
-}
