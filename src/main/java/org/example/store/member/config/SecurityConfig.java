66e4c36b9c5b55cf9edcadd36939562bc50e117e
leehansae1 <crof672@naver.com>
2025-02-03 14:40:25 +0900

    develop member & chatting

--- src/main/java/org/example/store/member/config/SecurityConfig.java
+++ src/main/java/org/example/store/member/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.example.store.member.config;
+//package org.example.store.member.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
